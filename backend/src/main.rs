79d8a8ba6bc2c33273b26deabb49fb4b02b40485
Anonymous Developer <hello@example.org>
2025-04-16 20:50:14 +0100

    feat(backend): command line argument parsing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,13 +4,35 @@
 #![deny(missing_docs)]
 
 use axum::Router;
+use clap::Parser;
+
+/// Command-line arguments of the application.
+#[derive(Parser, Debug, Clone)]
+struct Opt {
+    /// Address at which to serve the application.
+    #[clap(default_value = "0.0.0.0:8080")]
+    service_address: String,
+    /// Address to contact the Postgres server on.
+    #[clap(long)]
+    db_address: String,
+    /// Name of the database to open in Postgres.
+    #[clap(long, default_value = "tasks_db")]
+    db_name: String,
+    /// Enable verbose logging.
+    #[clap(short, long, default_value_t = false)]
+    verbose: bool,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let opts = Opt::parse();
+
     let app = Router::new();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(opts.service_address)
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
