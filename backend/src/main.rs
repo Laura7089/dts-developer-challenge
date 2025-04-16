696adc0673b22bffe4fbd73c3c908d8966fdc871
Anonymous Developer <hello@example.org>
2025-04-16 12:54:04 +0100

    refactor!(backend): move own folder, implement skeleton axum app

---
+++ backend/src/main.rs
@@ -0,0 +1,11 @@
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
